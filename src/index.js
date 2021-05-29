9239333431b2871162d83adf817f930daadbbec1
webiepscf <50769604+webiepscf@users.noreply.github.com>
2021-05-29 11:10:43 +0200

    Ajout d'une tâche à la todo-list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import "./styles.scss";
+
+// Eléments utiles
+const newTodoInput = document.querySelector(".new-todo");
+const todoListElt = document.querySelector(".todo-list");
+const todoCountElt = document.querySelector(".todo-count");
+renderNotCompletedCount();
+
+newTodoInput.onkeyup = function (e) {
+  if (e.key === "Enter") {
+    const newTodo = document.createElement("li");
+    newTodo.innerHTML = `
+          <div class="view">
+            <input class="toggle" type="checkbox">
+            <label>${this.value}</label>
+            <button class="destroy"></button>
+          </div>
+    `;
+    todoListElt.append(newTodo);
+    this.value = "";
+    renderNotCompletedCount();
+  }
+};
+
+function renderNotCompletedCount() {
+  const notCompletedCount = document.querySelectorAll(
+    ".todo-list li:not(.completed)"
+  ).length;
+  todoCountElt.innerText = notCompletedCount;
+}
